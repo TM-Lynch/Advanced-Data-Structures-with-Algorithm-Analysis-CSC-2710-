89e0f3cd7ef4ecbbb7c5cf8d85b5006c20d97c77
Taylor Lynch <77633606+TM-Lynch@users.noreply.github.com>
2021-12-20 17:23:21 -0500

    Add files via upload

---
+++ TestQueries.sql
@@ -0,0 +1,7 @@
+select * from conditioning_attendees;
+select * from frequentattendees;
+select * from manager_and_employees;
+select * from manager_wages_earned;
+select * from olias_attendees;
+select * from sum_balance;
+select * from top_vendors_for_gym_3;
